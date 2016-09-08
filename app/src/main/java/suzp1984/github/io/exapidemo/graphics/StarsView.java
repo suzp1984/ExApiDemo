170c36645c8424642402fc4e570cdb51667e2db4
suzp1984 <suzp1984@gmail.com>
2016-09-08 08:13:14 +0800

    StarsView dev

--- app/src/main/java/suzp1984/github/io/exapidemo/graphics/StarsView.java
+++ app/src/main/java/suzp1984/github/io/exapidemo/graphics/StarsView.java
@@ -2,8 +2,12 @@ package suzp1984.github.io.exapidemo.graphics;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -11,7 +15,10 @@ import android.view.View;
  */
 public class StarsView extends View {
 
-    private Paint mTxtPaint;
+    private static final String TAG = "StarsView";
+
+    private Paint mStarPaint;
+    private Path  mPath;
 
     public StarsView(Context context) {
         super(context);
@@ -40,10 +47,12 @@ public class StarsView extends View {
     @Override
     public void onSizeChanged(int w, int h, int oldw, int oldh) {
         // super.onSizeChanged(w, h, oldw, oldh);
+        Log.e(TAG, "onSizeChanged: " + w + ", " + h + ", " + oldw + ", " + oldh);
     }
 
     @Override
     public void onMeasure(int widthMeasureSpec, int hightMeasureSpec) {
+        Log.e(TAG, "onMeasure: " + widthMeasureSpec + ", " + hightMeasureSpec);
 
         int minW = getPaddingLeft() + getPaddingRight() + getSuggestedMinimumWidth();
         int w = resolveSizeAndState(minW, widthMeasureSpec, 1);
@@ -56,15 +65,71 @@ public class StarsView extends View {
 
     @Override
     public void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+        // super.onDraw(canvas);
+        Log.e(TAG, "onDraw");
 
-        mTxtPaint.setTextSize(40f);
-        canvas.drawText("hello world", 190, 190, mTxtPaint);
+        canvas.save();
+        canvas.translate(200, 200);
+        // canvas.rotate(45f);
+        // canvas.scale(200, 200);
+
+        // canvas.drawRect(0, 0, 1, 1, mStarPaint);
+        mPath.reset();
+        mPath.moveTo(0, 0);
+        mPath.lineTo(100, 100);
+        // mPath.moveTo(3, 2);
+        mPath.lineTo(100, 0);
+        // mPath.moveTo(3, 3);
+        mPath.lineTo(0, 0);
+        mPath.close();
+
+        mStarPaint.setStrokeWidth(1f);
+        mStarPaint.setDither(false);
+
+        canvas.drawPath(mPath, mStarPaint);
+
+        canvas.restore();
+        // drawStar(canvas, 100, 100, 100, 0);
     }
 
     private void init() {
-        mTxtPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mTxtPaint.setColor(0xffffff00);
-        mTxtPaint.setStyle(Paint.Style.STROKE);
+        mStarPaint = new Paint();
+        mStarPaint.setAntiAlias(true);
+        mStarPaint.setStrokeWidth(1f);
+        // mStarPaint.setDither(true);
+        // mStarPaint.setStrokeJoin(Paint.Join.ROUND);
+        // mStarPaint.setStrokeCap(Paint.Cap.ROUND);
+        mStarPaint.setColor(Color.rgb(255, 0, 0));
+        mStarPaint.setARGB(255, 255, 0, 0);
+        mStarPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        mPath = new Path();
+
+    }
+
+    private void drawStar(Canvas canvas, int x, int y, int r, int a) {
+        canvas.save();
+
+        canvas.translate(x, y);
+        // canvas.scale(r, r);
+
+        drawStandardStar(canvas);
+        canvas.drawPath(mPath, mStarPaint);
+
+        canvas.restore();
+    }
+
+    private void drawStandardStar(Canvas canvas) {
+        mPath.reset();
+        mPath.moveTo(0, 0);
+        for (int i = 0; i < 5; i++) {
+            mPath.lineTo((float)Math.cos((18 + i * 72)/180 * Math.PI) * 200f,
+                    (float)-Math.sin((18 + i * 72)/180 * Math.PI) * 200f);
+
+            mPath.lineTo((float)Math.cos((54 + i * 72)/180 * Math.PI) * 0.5f * 200f,
+                    (float)-Math.sin((54 + i * 72)/180 * Math.PI) * 0.5f * 200f);
+        }
+
+        mPath.close();
     }
 }
