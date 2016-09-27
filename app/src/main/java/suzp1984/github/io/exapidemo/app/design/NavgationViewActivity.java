fe0e911bbd639bd38114a112c96f768f6c726b5b
suzp1984 <suzp1984@gmail.com>
2016-09-27 14:23:14 +0800

    add navigatin sample

---
+++ app/src/main/java/suzp1984/github/io/exapidemo/app/design/NavgationViewActivity.java
@@ -0,0 +1,15 @@
+package suzp1984.github.io.exapidemo.app.design;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import suzp1984.github.io.exapidemo.R;
+
+public class NavgationViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navgation_view);
+    }
+}
