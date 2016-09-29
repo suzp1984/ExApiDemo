7e2ce32a2925aed49a0b727a84033b3c062cc0dc
suzp1984 <suzp1984@gmail.com>
2016-09-29 17:14:08 +0800

    Navigation View dev

--- app/src/main/java/suzp1984/github/io/exapidemo/app/design/NavgationViewActivity.java
+++ app/src/main/java/suzp1984/github/io/exapidemo/app/design/NavgationViewActivity.java
@@ -1,9 +1,14 @@
 package suzp1984.github.io.exapidemo.app.design;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,6 +19,15 @@ public class NavgationViewActivity extends AppCompatActivity {
     @BindView(R.id.recycler)
     RecyclerView mRecycler;
 
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawer;
+
+    @BindView(R.id.menu_list)
+    RecyclerView mMenuList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +35,21 @@ public class NavgationViewActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+            mToolbar.setNavigationIcon(R.mipmap.ic_launcher);
+            mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mDrawer.openDrawer(Gravity.LEFT);
+                }
+            });
+        }
+
         mRecycler.setAdapter(new HeaderAdapter());
         mRecycler.setLayoutManager(new LinearLayoutManager(this));
 
+        mMenuList.setAdapter(new HeaderAdapter());
+        mMenuList.setLayoutManager(new LinearLayoutManager(this));
     }
 }
